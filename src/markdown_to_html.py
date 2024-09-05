e3b52c5cd78dff65d2cf55606204800c00be4289
justinjest <jest.cayaban@gmail.com>
2024-09-05 21:51:56 +0900

    Worked on markdown to html some more

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -3,14 +3,27 @@ from htmlnode import HTMLNode
 from parentnode import ParentNode
 from textnode import TextNode
 from split_md import *
+from leafnode import LeafNode
 
 def markdown_to_html_node(markdown):
+    # Markdown -> Blocks
+    # Blocks -> TextNode
+    # TextNode -> HTML node
+    # For basic textbolcks we wrap this with <p>
+    block_wrapper={"quote":"blockquote",
+                   "unordered_list":"ul",
+                   "ordered_list":"ol",
+                   "code":"code",
+                   "heading":"h1",
+                   "paragraph":"p"}
     blocks = markdown_to_blocks(markdown)
     for block in blocks:
+        # Each block must now be converted into the parent HTML item
+        block_type = block_to_block_type(block)
         text_nodes = text_to_textnodes(block)
-    # TODO: Insert ParentNode that surrounds text_node
-    html_nodes = []
-    for text_node in text_nodes:
-        html_nodes.append(text_node.text_node_to_html_node())
-
-    return (html_nodes)
+        leaf_nodes = []
+        for text_node in text_nodes:
+            # This creates the LeafNodes that we need for this
+            leaf_nodes.append(text_node.text_node_to_html_node())
+        node = ParentNode(f"{block_wrapper[block_type]}", leaf_nodes)
+        print(node.to_html())
--- src/test_markdown_to_html.py
+++ src/test_markdown_to_html.py
@@ -5,6 +5,13 @@ class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
         test = markdown_to_html_node("Simple text")
         print(test)
+    
+        test = markdown_to_html_node("""Slightly more complicated text.
 
+two sentences""")
+        print(test)
+
+        test = markdown_to_html_node("""This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)""")
+        print(test)
 if __name__ == "__main__":
     unittest.main()
