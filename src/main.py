4e4b9a8627b7c55316a9fbbd63f22b13d0b062cc
justinjest <jest.cayaban@gmail.com>
2024-09-14 21:45:39 +0900

    Finished core functionality, passed all tests. It can now generate a simple webpage that works out of the box.

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ def main():
     static_path = "static"
     empty_folder(public_path)
     copy_static(static_path, public_path)
-    generate_page("content/index.md", "template.html", public_path)
+    generate_page_recursive("content", "template.html", public_path)
 
 # Path -> None
 # Deletes everything in Path to allow us to create new folders
@@ -70,6 +70,42 @@ def generate_page(from_path, template_path, dest_path):
     with open(f"{dest_path}/index.html", "w") as output:
         output.write(html_template)
     output.close()
+
+# Path, Template, Path
+# Takes all MD from from_path and it's sub folders, 
+# html template from template_path, and copies new 
+# HTML doc to dest_path
+def generate_page_recursive(from_path, template_path, dest_path):
+    for item in os.listdir(from_path):
+        path = os.path.join(from_path, item)
+        if os.path.isfile(path):
+            print(f"Generating page from {path} to {dest_path} using {template_path}")
+            with open(path) as md:
+                markdown = md.read()
+            md.close()
+            with open(template_path) as template:
+                html_template = template.read()
+            template.close()
+
+
+            Content = markdown_to_html_node(markdown)
+            Title = extract_title(markdown)
+            html_template = html_template.replace("{{ Title }}", Title)
+            html_template = html_template.replace("{{ Content }}", Content)
+            if not os.path.exists(dest_path):
+                os.mkdir(dest_path)
+            print(f"Creating {path}")
+            with open(f"{dest_path}/index.html", "w") as output:
+                output.write(html_template)
+            output.close()
+        else:
+            new_dest = os.path.join(dest_path, item)
+            new_from = os.path.join(from_path, item)
+            print (f"{path} is a subfolder, moved to {new_dest}")
+            os.mkdir(new_dest)
+            generate_page_recursive(new_from, template_path, new_dest)
+
+
 
 
 if __name__ == "__main__":
